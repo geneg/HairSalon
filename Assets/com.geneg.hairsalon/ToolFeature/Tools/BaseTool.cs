52706c15b54a50ba3200d0d686d6bec6321485d2
Eugene Girenko <vollcanoflash@gmail.com>
2024-04-20 17:20:50 +0300

    DryerTool

--- Assets/com.geneg.hairsalon/ToolFeature/Tools/BaseTool.cs
+++ Assets/com.geneg.hairsalon/ToolFeature/Tools/BaseTool.cs
@@ -8,80 +8,23 @@ namespace com.geneg.hairsalon.ToolFeature.Tools
 	public abstract class BaseTool : MonoBehaviour, ITool
 	{
 		[SerializeField] protected SpriteRenderer _spriteRenderer;
-		
-		private bool _isFirstTimeAngleCalculation;
-		private float _initialAngle;
-		private Vector2 _spritePivot;
+		private bool _initialized;
 
 		public string Name { get; set; }
-		public void Init()
+		
+		public virtual void Init()
 		{
-			_isFirstTimeAngleCalculation = true;
 			_spriteRenderer.sortingLayerName = "tools";
 			_spriteRenderer.color = Utils.ChangeAlpha(_spriteRenderer.color, 1f);
-			_spritePivot = _spriteRenderer.sprite.pivot.normalized;
+			_initialized = true;
 		}
 
-		private void Update()
+		protected virtual void Update()
 		{
-			Vector3 mousePosition;
-			
-			if (_spriteRenderer.flipX)
-			{
-				mousePosition = Input.mousePosition + new Vector3(-1, 0, 0);;
-			}
-			else
-			{
-				mousePosition = Input.mousePosition + new Vector3(1, 0, 0);;
-			}
-			
-			mousePosition.z = 10; // Set this to the distance between the camera and the objects it is rendering
-			Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-
-			if (_spriteRenderer.flipX)
-			{
-				transform.position = worldPosition + new Vector3(_spritePivot.x, _spritePivot.y, 0) - new Vector3(1, 0, 0);
-			}
-			else
-			{
-				transform.position = worldPosition + new Vector3(_spritePivot.x, _spritePivot.y, 0);	
-			}
+			//prevent the tool from being updated if it has not been initialized
+			if (!_initialized) return;
 			
 			
-			// Update the rotation of the tool to match the mouse position
-			// Calculate the direction vector from the tool's position to the target position
-			Vector3 direction;
-				
-			if (_spriteRenderer.flipX)
-				direction =  new Vector3(-1,1.1f,0) - transform.position;
-			else
-			    direction =  new Vector3(1,1.1f,0) - transform.position;
-
-			// Calculate the rotation angle from the direction vector
-			float angle = Mathf.Atan2(-direction.y, direction.x) * Mathf.Rad2Deg;
-			
-			if (_isFirstTimeAngleCalculation)
-			{
-				// Save the initial angle
-				_initialAngle = angle;
-				_isFirstTimeAngleCalculation = false;
-			}
-
-			if(angle < 90 && angle > -90)
-			{
-				_spriteRenderer.flipX = false;
-				
-				
-				transform.rotation = Quaternion.AngleAxis(_initialAngle - angle, Vector3.forward);
-			}
-			else
-			{
-				_spriteRenderer.flipX = true;
-				
-				transform.rotation = Quaternion.AngleAxis((180 - _initialAngle) - angle, Vector3.forward);
-			}
-			
-			// Apply the rotation to the tool
 			
 		}
 	}
--- Assets/com.geneg.hairsalon/ToolFeature/Tools/DryerTool.cs
+++ Assets/com.geneg.hairsalon/ToolFeature/Tools/DryerTool.cs
@@ -1,12 +1,77 @@
 
-using System.Numerics;
-using com.geneg.hairsalon.Common;
+using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
 
 namespace com.geneg.hairsalon.ToolFeature.Tools
 {
 	public class DryerTool : BaseTool
 	{
+		[SerializeField] private float _centerOffset = 1f;
+			
+		private bool _isFirstTimeAngleCalculation;
+		private float _initialAngle;
+		private Vector2 _spritePivot;
 		
+		public override void Init()
+		{
+			_isFirstTimeAngleCalculation = true;
+			_spritePivot = _spriteRenderer.sprite.pivot.normalized;
+			
+			base.Init();
+		}
+
+		protected override void Update()
+		{
+			base.Update();
+			
+			Vector3 mousePosition;
+			Vector3 direction;
+			
+			if (_spriteRenderer.flipX)
+			{
+				mousePosition = Input.mousePosition + new Vector3(-_centerOffset, 0, 0);;
+			}
+			else
+			{
+				mousePosition = Input.mousePosition + new Vector3(_centerOffset, 0, 0);;
+			}
+			
+			mousePosition.z = 10; // Set this to the distance between the camera and the objects it is rendering
+			Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+			
+			//position of the tool is update to match the mouse position, taking into account the pivot of the sprite
+			if (_spriteRenderer.flipX)
+			{
+				transform.position = worldPosition + new Vector3(_spritePivot.x, _spritePivot.y, 0) + new Vector3(-_centerOffset, 0, 0);
+				direction =  new Vector3(-_centerOffset,1.1f,0) - transform.position;
+			}
+			else
+			{
+				transform.position = worldPosition + new Vector3(_spritePivot.x, _spritePivot.y, 0) + new Vector3(_centerOffset, 0, 0);
+				direction =  new Vector3(_centerOffset,1.1f,0) - transform.position;
+			}
+			
+			float angle = Mathf.Atan2(-direction.y, direction.x) * Mathf.Rad2Deg;
+			
+			//if this is the first time the angle is being calculated. If it is, the initial angle is saved for future calculations
+			if (_isFirstTimeAngleCalculation)
+			{
+				_initialAngle = angle;
+				_isFirstTimeAngleCalculation = false;
+			}
+
+			//ensures that the tool always faces the correct direction based on the mouse position.
+			if(angle < 90 && angle > -90)
+			{
+				_spriteRenderer.flipX = false;
+				transform.rotation = UnityEngine.Quaternion.AngleAxis(_initialAngle - angle, Vector3.forward);
+			}
+			else
+			{
+				_spriteRenderer.flipX = true;
+				transform.rotation = UnityEngine.Quaternion.AngleAxis(180 - _initialAngle - angle, Vector3.forward);
+			}
+			
+		}
 	}
 }
